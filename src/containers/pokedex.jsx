378babfbb59eb1fbd8dcf722daf9ff600bda8b0e
Jenni <jennimacklin@gmail.com>
2018-10-26 16:33:33 -0400

    initial commit

---
+++ src/containers/pokedex.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Pokedex extends React.Component {
+    render() {
+        return(
+            <div id="pokedex">
+                <p>This is a pokedex. Here are some pokemon.</p>
+            </div>
+        );
+    }
+}
+
+export default Pokedex;
