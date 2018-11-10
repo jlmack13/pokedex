782c5f09ddc65ffb69c110649068305237e3d336
jlmack13 <jennimacklin@gmail.com>
2018-11-09 20:42:52 -0500

    adding styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 1.5rem;
+    text-align: center;
+    color: palevioletred;
+`
 
 const Header = () => {
     return (
         <div>
-            <h1>Pokedex</h1>
+            <Title>Pokedex</Title>
         </div>
     );
 };
