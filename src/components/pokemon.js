ab9db61f9cd3e10c611743b50677fc5a6d5f4196
jlmack13 <jennimacklin@gmail.com>
2018-11-20 15:20:23 -0500

    position dex number diagonally right and above name

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -19,29 +19,31 @@ const Name = styled.p`
     text-transform: uppercase;
     font-family: 'PokemonGB';
     color: black;
-    margin-top: 30px;
     font-size: 16px;
     font-weight: bold;
     text-shadow: 1px 1px rgba(0, 0, 0, .3);
+    text-align: center;
 `
 
 const Number = styled.p`
     font-weight: bold;
     color: blue;
     font-size: 20px;
+    text-align: right;
+    padding-bottom: 1rem;
 `
 const PokemonHeader = styled.div`
     display: grid;
-    grid-template-columns: 150px 50px;
+    grid-template-rows: 30px 50px;
 `
 
 const Pokemon = ({name, dexNumber, imageURL, description, type}) => {
     return (
         <Card>
             <PokemonHeader>
-                <Name>{name}</Name>
                 {/* convert the dex number to a string, and pad it with 0s until its 3 digits so it matches conventional formating of pokedex numbers*/}
                 <Number>{dexNumber.toString().padStart(3, "0")}</Number>
+                <Name>{name}</Name>
             </PokemonHeader>
             <StyledImage src={imageURL} alt={name}/>
             <Type type={type} />
